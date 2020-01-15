4dcbe3d377613ebba549cd4bfc4f0b1580c03683
Samuel Suh <samsuh@gmail.com>
2020-01-15 15:22:39 -0800

    imported and setup redux-thunk in preparation for writing action creators

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter, Route} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
+import reduxThunk from 'redux-thunk'
 
 import reducers from './reducers'
 import App from './components/App'
 import Landing from './components/Landing'
 import Signup from './components/auth/Signup'
 
+//import redux-thunk and tie it to the createStore call. expanded from just "createStore(reducers, {})"
+const store = createStore(
+    reducers, 
+    //initial state
+    {},
+    applyMiddleware(reduxThunk)
+)
+
 ReactDOM.render(
-    //Provider and createStore wrap BrowserRouter, and we pass 'store' prop to the Provider as createStore
+    //Provider with createStore wraps BrowserRouter, and we pass 'store' prop to the Provider as createStore. expanded to accomodate redux-thunk middleware, pulled it out to a constant defined above.
     //we pass in our reducers and a default {}
-    <Provider store={createStore(reducers, {})}>        
+    <Provider store={store}>        
         <BrowserRouter>
             <App>
                 <Route path="/" exact component={Landing} />
